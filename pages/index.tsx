2d6886a716f7774da75244deec95a461340d4685
donguk lee <novelview9@gmail.com>
2022-03-31 14:28:28 +0900

    Update scale

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,12 @@ import { useEffect, useState } from "react";
 import useWebSocket from 'react-use-websocket';
 
 
-
-
-
 const IndexPage = () => {
   const [data, setData] = useState<any>(["test"]);
   const { lastMessage, readyState } = useWebSocket("ws://127.0.0.1:2012");
+  useEffect(() => {
+    console.log(readyState)
+  }, [readyState]);
   useEffect(() => {
     setData((prev) => [...prev, lastMessage]);
   }, [lastMessage]);
